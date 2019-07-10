d76646185cd27a5ad246ac4618b7f5ca25d1fdee
DanielJAch <danieljach@gmail.com>
2019-07-10 11:56:53 -0700

    More cleanup

--- server/app.js
+++ server/app.js
@@ -1,17 +1,18 @@
 'use strict';
 
-var PORT = process.env.PORT;
-var PORT_HTTPS = process.env.PORT_HTTPS;
+const PORT = process.env.PORT || 5000;
+const PORT_HTTPS = process.env.PORT_HTTPS || 5001;
+const HOST = process.env.HOST || 'localhost';
 
 console.log('------------Starting App in %s Environment------------', process.env.NODE_ENV);
 
-var express = require('express');
-var https = require('https');
-var config = require('./config');
-var middleware = require('./core/middleware');
-var bodyParser = require('body-parser');
-var helmet = require('helmet');
-var app = express();
+const express = require('express');
+const https = require('https');
+const config = require('./config');
+const middleware = require('./core/middleware');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const app = express();
 
 // Body parsers - support parsing of JSON or UTF-8 urlencoded bodies (e.g. from a form).
 app.use(bodyParser.json({ limit: 52428800 }));
@@ -30,9 +31,9 @@ require('./core/routes')(app);
 // Error Handling (must be the last middleware / app.use() call)
 app.use(middleware.errorHandler);
 
-var server = app.listen(PORT, function() {
-  var host = server.address().address;
-  var port = server.address().port;
+const server = app.listen(PORT, HOST, function() {
+  const host = server.address().address;
+  const port = server.address().port;
 
   console.log('Express server listening at http://%s:%s', host, port);
 });
